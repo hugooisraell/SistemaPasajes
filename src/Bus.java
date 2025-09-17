106143294808273afb8d7426b9999d72ddc6da3b
Hugo Lainez <hugoolai05@gmail.com>
2025-09-16 23:11:14 -0500

    Se añade un constructor, setters y getters

--- src/Bus.java
+++ src/Bus.java
@@ -1,16 +1,52 @@
 /**
  * Proyecto: SistemaPasajes
  * Autor: Hugo Israel Láinez Medina
- * DNI: 2400396574
+ * ID: 2400396574
  * Fecha de creación: domingo, 14 de septiembre de 2025
- * Última modificación: lunes, 15 de septiembre de 2025
  */
 
-class Bus {
+public class Bus {
 
-    int numero; // numero/disco de bus
-    String placa; // numero de placa
-    int capacidad; // capacidad del bus
-    boolean[] asientos =  new boolean[capacidad]; // identificar asiento ocupado o no
+    private int numero; // numero/disco de bus
+    private String placa; // numero de placa
+    private int capacidad; // capacidad del bus
+    private boolean[] asientos =  new boolean[capacidad]; // identificar asiento ocupado o no
 
+    public Bus(int numero, String placa, int capacidad) {
+        this.numero = numero;
+        this.placa = placa;
+        this.capacidad = capacidad;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+
+    public int getCapacidad() {
+        return capacidad;
+    }
+
+    public void setCapacidad(int capacidad) {
+        this.capacidad = capacidad;
+    }
+
+    public boolean[] getAsientos() {
+        return asientos;
+    }
+
+    public void setAsientos(boolean[] asientos) {
+        this.asientos = asientos;
+    }
 }
